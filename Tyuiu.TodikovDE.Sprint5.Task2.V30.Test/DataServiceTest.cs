28816cbe4f5127df5930f38c712a0b76a1fa2bef
TodikovDanil <todic.danil@gmail.com>
2024-11-22 21:34:13 +0300

    Task2

---
+++ Tyuiu.TodikovDE.Sprint5.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.TodikovDE.Sprint5.Task2.V30.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+namespace Tyuiu.TodikovDE.Sprint5.Task2.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.TodikovDE.Sprint5.Task2.V30/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.TodikovDE.Sprint5.Task2.V30.Lib;
+namespace Tyuiu.TodikovDE.Sprint5.Task2.V30
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Тодиков Д. Э. | ИСТНб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #5                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #2                                                             *");
+            Console.WriteLine("* Вариант #30                                                            *");
+            Console.WriteLine("* Выполнил: Тодиков Даниил Эдуардович | ИСТНб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+            int[,] mtrx = new int[3, 3] { { 3, -1, -3 },
+                                         {-2, -5, 0 },
+                                         { -8, -7, 2 }};
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine("***************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+                Console.WriteLine("***************************************************************************");
+
+                string res = ds.SaveToFileTextData(mtrx);
+
+                Console.WriteLine("Файл: " + res);
+                Console.WriteLine("Создан!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
