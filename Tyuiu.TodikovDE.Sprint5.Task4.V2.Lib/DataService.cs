f0e09ed638f140160d04ec2196f07469c73e7f74
TodikovDanil <todic.danil@gmail.com>
2024-11-24 13:48:51 +0300

    Task4

---
+++ Tyuiu.TodikovDE.Sprint5.Task4.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
+namespace Tyuiu.TodikovDE.Sprint5.Task4.V2.Lib
+{
+    public class DataService : ISprint5Task4V2
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+            string str1 = str.Replace(".", ",");
+            double x = Convert.ToDouble(str1);
+            double res = Math.Round((Math.Cos(x) + 3 * x)/(0.25 * x) + 4 - 2.1 * x, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.TodikovDE.Sprint5.Task4.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.TodikovDE.Sprint5.Task4.V2.Lib;
+namespace Tyuiu.TodikovDE.Sprint5.Task4.V2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Тодиков Д. Э. | ИСТНб-24-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спритн #5                                                              *");
+            Console.WriteLine("* Тема: Создания итогого решения по спринту                              *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #21                                                            *");
+            Console.WriteLine("* Выполнил: Тодиков Даниил Эдуардович | ИСТНб-24-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\User\source\repos\Tyuiu.TodikovDE.Sprint5\DataSprint5\InPutDataFileTask4V2.txt";
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
